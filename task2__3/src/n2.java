41e05f12b3204eae05fbf8075a39827e17381d30
ayanvrsn <129643419+ayanvrsn@users.noreply.github.com>
2025-01-09 18:15:55 +0500

    Add files via upload

---
+++ task2__3/src/n2.java
@@ -0,0 +1,8 @@
+public class Woman extends Human {
+}
+
+public class Terran {
+}
+
+public class Human extends Terran {
+}
---
+++ task2__3/src/n3.java
@@ -0,0 +1,12 @@
+public class ElectricCar extends Car {
+    int electricEnginePower;
+}
+
+public class Vehicle {
+    double maxSpeed;
+}
+
+public class Car extends Vehicle {
+    int wheelCount;
+    double weight;
+}
